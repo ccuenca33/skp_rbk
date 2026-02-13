e4d40960ce091c73e613f45b8e4861c4e6050cda
ccuenca33 <94115381+ccuenca33@users.noreply.github.com>
2026-02-13 08:09:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+let qrMap = {
+"P1": "SOL001",
+"P2": "SOL002",
+"P3": "SOL003",
+"P4": "SOL004",
+"P5": "SOL005",
+"P6": "SOL006"
+};
+
 let validCodes = {
 "SOL001": "Acceso válido",
 "SOL002": "Acceso válido",
@@ -7,13 +16,27 @@ let validCodes = {
 "SOL006": "Acceso válido"
 };
 
-function validate(){
-let code = document.getElementById("codeInput").value.trim();
-let result = document.getElementById("result");
+function getQRid(){
+ let params = new URLSearchParams(window.location.search);
+ return params.get("id");
+}
 
-if(validCodes[code]){
- result.innerHTML = "<div class='ok'>" + validCodes[code] + "</div>";
-}else{
- result.innerHTML = "<div class='fail'>❌ Código incorrecto</div>";
+window.onload = function(){
+ let id = getQRid();
+
+ if(id && qrMap[id]){
+   document.getElementById("codeInput").value = qrMap[id];
+ }
 }
+
+function validate(){
+ let code = document.getElementById("codeInput").value.trim().toUpperCase();
+ let result = document.getElementById("result");
+
+ if(validCodes[code]){
+   result.innerHTML = "<div class='ok'>" + validCodes[code] + "</div>";
+ }else{
+   result.innerHTML = "<div class='fail'>Código incorrecto</div>";
+ }
 }
+
